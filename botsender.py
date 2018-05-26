60f58d5ee7fef5b95cb7699cbec26c42f597655c
Jobsdone <tony.smirno@yandex.ru>
2018-05-27 00:22:19 +0300

    hidden the credentials

--- botsender.py
+++ botsender.py
@@ -1,5 +1,5 @@
 import telegram
-
+from credentials import tokens
 
 def send_warning(type_of_message, measure,*args, **kvargs):
     """
@@ -7,8 +7,9 @@ def send_warning(type_of_message, measure,*args, **kvargs):
     тип сообщения может быть предупреждением о превышении температуры цп или о превышении траффика загрузки
     дополнительно сообщает о размере превышения
     """
-    proxy_server = telegram.utils.request.Request(proxy_url='socks5://163.172.152.192:1080')
-    bot = telegram.Bot(token='594628795:AAHHgWe6wDZINYf-jPh11YK3R6-mgV3ElS8', request=proxy_server)
+    chat_id = tokens['chat_id']
+    proxy_server = telegram.utils.request.Request(proxy_url=tokens['proxy_url'])
+    bot = telegram.Bot(token=tokens['token'], request=proxy_server)
     if type_of_message == 'temperature_high':
         text_of_message = 'температура устройства {} высокая и составляет {}'.format(kvargs['name'], str(measure))
         bot.send_message(chat_id=350729653, text=text_of_message)
